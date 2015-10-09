002d95ceadbc6de5385d1c058532d2ec9429d565
czqInNanjing <1300236145@qq.com>
2015-10-09 16:14:40 +0800

    推一发~

--- XYZWorks/src/main.java
+++ XYZWorks/src/main.java
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
+
+
+
 
 public class main {
 public main() {
-	System.out.println();// TODO Auto-generated constructor stub
-}
-}
-=======
 
-public class main {
-public main() {
-<<<<<<< HEAD
 	System.out.println();// TODO Auto-generated constructor stub
-=======
+
 	System.out.println("asdasd");// TODO Auto-generated constructor stub
->>>>>>> origin/dev
-}
-}
->>>>>>> 354a828fa5a917daa72e69f9b3fc39566ab16c25
+
+}}
+
